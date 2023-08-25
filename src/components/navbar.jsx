29c245fa5301b5cbd078b4bd7af9d9f4df8221fd
Federico Suarez <fsuarez@intermediait.com>
2023-08-25 17:41:12 -0300

    update images

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,36 +1,32 @@
-import React from 'react';
-import './navbar.css';
+import React from "react";
+import "./navbar.css";
 
-export default function Navbar(){
- return (
-  <div className="heading">
-   <div id="logo">
- <img id= "logoImg" src="../../public/assets/logo.png" alt="" />  
- </div> 
+export default function Navbar() {
+  return (
+    <div className="heading">
+      <div id="logo">
+        <img id="logoImg" src="src/assets/logo.png" alt="" />
+      </div>
 
- <div id="menu">
+      <div id="menu">
+        <div id="menuPCBox">
+          <ul id="menuPC">
+            <li>
+              <a href="">denuncia</a>
+            </li>
+            <li>
+              <a href="">notas</a>
+            </li>
+            <li>
+              <a href="">nosotrxs</a>
+            </li>
+          </ul>
+        </div>
 
-<div id="menuPCBox">
-   <ul id="menuPC">
-        <li><a href="">denuncia</a></li>
-        <li><a href="">notas</a></li>
-        <li><a href="">nosotrxs</a></li>
-    </ul>
-</div>
-   
- 
-    <div id="botonMenu" >
-
-       <img id= "menuImg" src="../../public/assets/menu.png" alt="" />
-
-     </div>
- 
-
- </div>
-   
-
-   
-
-  </div>
- );
+        <div id="botonMenu">
+          <img id="menuImg" src="src/assets/menu.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }

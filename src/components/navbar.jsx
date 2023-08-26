ae89eb8f5c188c4e92c62a2286fa41973f607b43
FranScarvaci <frannobinarie@gmail.com>
2023-08-26 15:16:36 -0300

    implement-branch-router-dom-in-preProdu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,40 +1,46 @@
-import React from 'react';
+import * as React from "react";
 import './navbar.css';
+import {
+  Link,
+  Outlet,
+
+} from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div className="heading">
+      {/* Enlace a la ruta MAIN2 */}
+      <Link to="/mapa">
+        <div id="logo">
+          <img id="logoImg" src="../src/assets/logo.png" alt="" />
+        </div>
+      </Link>
+
+      <div id="menu">
+
+        <div id="menuPCBox">
+          <ul id="menuPC">
+          <Link to="/notas">Notas</Link>
+          <Link to="/conecta">Conecta</Link>
+            <Link to="/reporta">Reporta</Link>
+
+          </ul>
+        </div>
+
+
+        <div id="botonMenu" >
+
+          <img id="menuImg" src="../src/assets/menu.png" alt="" />
+
+        </div>
+
+
+      </div>
 
 
 
-export default function Navbar(){
- return (
-  <div className="heading">
-   <div id="logo">
- <img id= "logoImg" src="../src/assets/logo.png" alt="" />  
- </div> 
 
- <div id="menu">
+    </div>
 
-<div id="menuPCBox">
-   <ul id="menuPC">
-        <li><a href="">informes</a></li>
-        <li><a href="">nosotrxs</a></li>
-        <button id='botonReporta'>reportá</button>
-
-    </ul>
-</div>
-   
- 
-    <div id="botonMenu" >
-
-       <img id= "menuImg" src="../src/assets/menu.png" alt="" />
-
-     </div>
- 
-
- </div>
-   
-
-
-
-  </div>
-
- );
+  );
 }

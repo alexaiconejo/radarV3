3cdb58a34c9e3c91e8febeadafe6771b7970d7a7
alexaiconejo <fondo.gif>
2023-08-26 15:19:00 -0300

    markers-style

--- src/components/Markers.jsx
+++ src/components/Markers.jsx
@@ -1,8 +1,22 @@
 import { Marker } from "react-map-gl/maplibre";
 
+
+
+const tipoIdStyles = {
+  t1: "amarillo",
+  t2: "naranja",
+  t3: "rojo",
+};
+
+
+
+
 export const Markers = ({ events, setPopupInfo, setMarker, selected }) =>
   events.map((event, index) => {
-    const { coords, title, date, source, id } = event;
+    const { coords, title, date, source, id, tipoId } = event;
+    const markerStyle = `marker ${id === selected ? "hovered" : ""} ${
+      tipoIdStyles[tipoId]
+    }`;
     return (
       <div key={index}>
         <Marker
@@ -22,7 +36,7 @@ export const Markers = ({ events, setPopupInfo, setMarker, selected }) =>
             })
           }
         >
-          <div className={`marker ${id === selected ? "hovered" : ""}`}>
+          <div className={markerStyle}>
             <span>{id}</span>
           </div>
         </Marker>

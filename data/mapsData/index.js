714730b878584e44b9b08fca3f92131781b69fc4
alexaiconejo <fondo.gif>
2023-08-31 18:19:54 -0300

    Updates

--- data/mapsData/index.js
+++ data/mapsData/index.js
@@ -1,6 +1,6 @@
 import provincias from "./provincias.json";
 import departamentos from "./departamentos-argentina.json";
-import departamentosBsAs from "./departamentos-buenos_aires.json";
+import departamentosBsAs from "./departamentos-argentina.json";
 import rutas from './rutas.json'
 
 export { provincias, departamentos, departamentosBsAs, rutas };

8e920e54a8f0dd15e009df73d80aea70b9a339d8
FranScarvaci <frannobinarie@gmail.com>
2023-09-03 20:40:42 -0300

    rebase-aplicado

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -23,26 +23,9 @@ export default function NavbarBootstrap() {
             </Navbar.Brand>
             <Nav className="me-auto">
               <div id="menuPC">
-<<<<<<< HEAD
-<<<<<<< HEAD
+
               <Nav.Link to="./notas" as={Link} className="d-none d-lg-block">INVESTIGACIONES</Nav.Link>
               <Nav.Link to="./conecta" as={Link} className="d-none d-lg-block">NOSOTRXS</Nav.Link>
-=======
-=======
->>>>>>> 66920875c042f71d4047c2a3b04b3911cb039dd2
-                <Nav.Link href="/radarV3/notas" className="d-none d-lg-block">INVESTIGACIONES</Nav.Link>
-                <Nav.Link href="/radarV3/conecta" className="d-none d-lg-block">NOSOTRXS</Nav.Link>
-                <Nav.Link href="https://docs.google.com/forms/d/e/1FAIpQLSe5pKXuvkvSUiwI_YEq5OZrfXCmpcsDuz_6co_iS2Qags80_A/viewform" target="_blank" rel="noopener noreferrer">
-                  <motion.div whileHover={{
-                    scale: [1, 2, 2, 1, 1],
-                    rotate: [0, 0, 270, 270, 0],
-                    borderRadius: ["20%", "20%", "50%", "50%", "20%"],
-                  }}>REPORTÁ</motion.div>
-                </Nav.Link>
-<<<<<<< HEAD
->>>>>>> 6692087 (motion-incorporated-to-img-button)
-=======
->>>>>>> 66920875c042f71d4047c2a3b04b3911cb039dd2
               </div>
 
             </Nav>
